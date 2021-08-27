00eee5ccb2064e93c30b6d5ba9359e4d56da85d8
Andrés Villalobos <andresalvivar@gmail.com>
2021-08-27 00:11:40 -0600

    Start template, Improved instructions

--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,24 @@ fn main() {
                 .about("Create a Component & System")
                 .arg(Arg::with_name("name").help("Name").required(true).index(1))
                 .arg(
-                    Arg::with_name("output")
+                    Arg::with_name("include_start")
+                        .short("s")
+                        .help("Include void Start()"),
+                )
+                .arg(
+                    Arg::with_name("output_files")
                         .short("o")
                         .help("Create the .cs files"),
                 )
                 .arg(
-                    Arg::with_name("nocomp")
-                        .long("nocomp")
-                        .help("Ignore the Component"),
+                    Arg::with_name("no_system")
+                        .long("ns")
+                        .help("Ignore the System"),
                 )
                 .arg(
-                    Arg::with_name("nosys")
-                        .long("nosys")
-                        .help("Ignore the System"),
+                    Arg::with_name("no_component")
+                        .long("nc")
+                        .help("Ignore the Component"),
                 ),
         )
         .subcommand(SubCommand::with_name("update").about("Self updates to the latest version"))
@@ -39,34 +44,39 @@ fn main() {
 
     if let Some(matches) = matches.subcommand_matches("new") {
         let name = matches.value_of("name").unwrap();
-        let output = matches.is_present("output");
-        let nocomp = matches.is_present("nocomp");
-        let nosys = matches.is_present("nosys");
+        let start = matches.is_present("include_start");
+        let output = matches.is_present("output_files");
+        let nosys = matches.is_present("no_system");
+        let nocomp = matches.is_present("no_component");
 
-        let component = generate_component_string(name);
+        let system_file = format!("{}System.cs", name);
         let system = generate_system_string(name);
 
         let component_file = format!("{}.cs", name);
-        let system_file = format!("{}System.cs", name);
+        let component = if start {
+            get_extended_component(name)
+        } else {
+            get_component(name)
+        };
 
         if output {
             let current_dir = env::current_dir().unwrap();
 
-            if !nocomp {
-                write_file(component.as_str(), current_dir.join(&component_file));
-            }
-
             if !nosys {
                 write_file(system.as_str(), current_dir.join(&system_file));
             }
-        }
 
-        if !nocomp {
-            println!("\n\n{}", component);
+            if !nocomp {
+                write_file(component.as_str(), current_dir.join(&component_file));
+            }
         }
 
         if !nosys {
             println!("\n\n{}", system);
+        }
+
+        if !nocomp {
+            println!("\n\n{}", component);
         }
 
         if output {
@@ -75,16 +85,16 @@ fn main() {
                 println!();
             }
 
-            if !nocomp {
-                println!("{} generated", component_file);
-            }
-
             if !nosys {
                 println!("{} generated", system_file);
             }
+
+            if !nocomp {
+                println!("{} generated", component_file);
+            }
         }
 
-        if !output && (!nocomp || !nosys) {
+        if !output && (!nosys || !nocomp) {
             println!();
         }
         println!("\nDone!");
@@ -93,16 +103,37 @@ fn main() {
     if let Some(_matches) = matches.subcommand_matches("update") {
         println!();
 
-        match update() {
-            Ok(_) => {}
-            Err(_) => {
-                panic!("Error updating.")
-            }
+        if update().is_err() {
+            panic!("Error updating.")
         }
     }
 }
 
-fn generate_component_string(name: &str) -> String {
+fn generate_system_string(name: &str) -> String {
+    let template = r#"
+
+using System.Collections.Generic;
+using UnityEngine;
+
+// #jam
+public class @ComponentSystem : MonoBehaviour
+{
+    public static List<@Component> components = new List<@Component>();
+
+    private void Update()
+    {
+        foreach (var c in components)
+        {
+
+        }
+    }
+}
+"#;
+
+    template.replace("@Component", name).trim().to_string()
+}
+
+fn get_component(name: &str) -> String {
     let template = r#"
 
 using UnityEngine;
@@ -125,23 +156,29 @@ public class @Component : MonoBehaviour
     template.replace("@Component", name).trim().to_string()
 }
 
-fn generate_system_string(name: &str) -> String {
+fn get_extended_component(name: &str) -> String {
     let template = r#"
 
-using System.Collections.Generic;
 using UnityEngine;
 
 // #jam
-public class @ComponentSystem : MonoBehaviour
+public class @Component : MonoBehaviour
 {
-    public static List<@Component> entities = new List<@Component>();
+    public Transform collider;
 
-    private void Update()
+    private void Start()
     {
-        foreach (var e in entities)
-        {
+        collider = GetComponent<Transform>();
+    }
 
-        }
+    private void OnEnable()
+    {
+        @ComponentSystem.entities.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        @ComponentSystem.entities.Remove(this);
     }
 }
 "#;
